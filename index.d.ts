5213b01d95a654e0e7089f6f9e195d5fa0db6c6c
thefliik <12599934+thefliik@users.noreply.github.com>
2020-12-09 01:21:06 -0800

    Add type definition file for typescript
    This small change adds typescript typing support.

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Performs a deep (recursive) comparison between the two arguments.
+ * This function requires that the two values have the same prototype 
+ * and properies, including unenumerable ones. Otherwise it returns
+ * false.
+ */
+export function isEqual<T>(a: T, b: T): boolean;
